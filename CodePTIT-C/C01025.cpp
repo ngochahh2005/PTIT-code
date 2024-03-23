b501b0f1724e5f5791173eae392ea479dd7a3393
NT Ngọc Hà <153905286+ngochahh2005@users.noreply.github.com>
2024-03-23 14:50:34 +0700

    C01025.cpp

--- CodePTIT-C/C01025.cpp
+++ CodePTIT-C/C01025.cpp
@@ -1,27 +1,25 @@
-#include <stdio.h>
+#include <stdio.h> 
 
 int max(int a, int b) {
-    return (a > b) ? a : b;
+	return (a > b) ? a : b;
 }
 
 int min(int a, int b) {
-    return (a < b) ? a : b;
+	return (a < b) ? a : b;
 }
 
-int main()
-{
-    int a[4][2];
-    int xMin = 100, xMax = -100;
-    int yMin = 100, yMax = -100;
-    for (int i = 0; i < 4; i++)
-    {
-        scanf("%d%d", &a[i][0], &a[i][1]);
-        xMin = min(xMin, a[i][0]);
-        xMax = max(xMax, a[i][0]);
-        yMin = min(yMin, a[i][1]);
-        yMax = max(yMax, a[i][1]);
-    }
-    int vl = max(xMax - xMin, yMax - yMin);
-    printf("%d", vl * vl);
-    return 0;
+int main() {
+	int xmax = -100, ymax = -100, xmin = 100, ymin = 100;
+	int x, y;
+	for (int i = 0; i < 4; i++) {
+		scanf("%d%d", &x, &y);
+		xmax = max(xmax, x);
+		ymax = max(ymax, y);
+		xmin = min(xmin, x);
+		ymin = min(ymin, y);
+	}
+	x = xmax - xmin;
+	y = ymax - ymin;
+	int ma = max(x, y);
+	printf("%d", ma*ma);
 }
